c6704a9f244089599fd56da215dc3f766098b05e
Nuno Rego <nuno.rego@outlook.pt>
2020-03-27 17:22:29 +0000

    PHP Migration is working

--- implementacao_g12/origin_server/php_migration_files/Migrate.php
+++ implementacao_g12/origin_server/php_migration_files/Migrate.php
@@ -8,7 +8,6 @@
             $fn = fopen($p,"r");
             $r = fgets($fn);
             fclose($fn);
-            echo $r;
             return $r;
             
          } else {
@@ -66,21 +65,36 @@
     *    3. Set ID of the last inserted log to the $originalId variable.
    */
    function migrateLogs($conn1, $conn2) {
-      $new_logs = $conn1->query("SELECT * FROM Log WHERE Log.idLog > " . getOriginalID());
+      echo "<b>Starting Log Migration...</b> </br></br>";
+      $new_logs = $conn1->query("SELECT * FROM log WHERE log.idLog > " .getOriginalID(). ";");
+
       if($new_logs->num_rows > 0) {
+         echo "<i>Successfully imported data from origin.</i></br></br>";
+
          while($row = $new_logs->fetch_assoc()) {
-            $insert_query = "INSERT INTO Log VALUES (" .$row["idLog"]. ", " .$row["tabela_chave"]. ", " .$row["coluna_chave"]. ", " .$row["id_chave"]. ", " .$row["valor_antigo"]. ", " .$row["valor_novo"]. ", " .$row["Tempo"]. ", " .$row["Op"]. ")";
+            $insert_query = "INSERT INTO g12_log VALUES ('" .$row["idLog"]. "', '" .$row["tabela_chave"]. "', '" .$row["coluna_chave"]. "', '" .$row["id_chave"]. "', '" .$row["valor_antigo"]. "', '" .$row["valor_novo"]. "', '" .$row["Tempo"]. "', '" .$row["Op"]. "')";
             $insert_new_log = $conn2->query($insert_query);
+
+            //Check if log export insertion went well
+            if ($insert_new_log === TRUE) {
+               echo "<i>Successfully exported Log with id: " . $row["idLog"]. "</i></br>";
+            } else {
+               echo "Error: INSERT INTO g12_log VALUES ('" .$row["idLog"]. "', '" .$row["tabela_chave"]. "', '" .$row["coluna_chave"]. "', '" .$row["id_chave"]. "', '" .$row["valor_antigo"]. "', '" .$row["valor_novo"]. "', '" .$row["Tempo"]. "', '" .$row["Op"]. "') <br>" . $conn2->error;
+            }
+
+            setOriginalID(intval($row["idLog"]));
          }
-         setOriginalID($row["idLog"]);
       }
+      
+      echo "</br>";
+      echo "<i>Log Migration has finished</i>";
    }
 
    /********************************Loop in which the DB Migration logic will occur********************************/
    
    //Open DB Connections
-   $auditor_conn = connectToDB("", "", "", ""); // Johnny [Auditor]
-   $origin_conn = connectToDB("", "", "", ""); // Localhost [Origin]
+   $auditor_conn = connectToDB("johnny.heliohost.org", "dctidata_g12", "senhag12", "dctidata_g12"); // Johnny [Auditor]
+   $origin_conn = connectToDB("localhost", "root", "teste123", "museum"); // Localhost [Origin]
 
    //Log Migration
    migrateLogs($origin_conn, $auditor_conn);
