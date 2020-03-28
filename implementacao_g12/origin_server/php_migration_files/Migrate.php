7a12838ece828aa88ab6e51028b83b4a88fca649
Nuno Rego <nuno.rego@outlook.pt>
2020-03-28 18:57:01 +0000

    Changed last id architecture

--- implementacao_g12/origin_server/php_migration_files/Migrate.php
+++ implementacao_g12/origin_server/php_migration_files/Migrate.php
@@ -1,5 +1,6 @@
 <?php
    /*********************************************Auxiliar Functions************************************************/
+   /*Old functions
    //Auxiliar Functions that read and write to the configuration files [$u_param must be "id" or "time" and $u_data must be an int]
    function readConfig($u_param) {
       try {
@@ -37,7 +38,6 @@
       $t = readConfig("time") ;
       return $t == "" ? 2 * 60 * 60 : intVal($t) * 60 * 60;
    }
-
    //Auxiliar Functions and variable to aid in migrating the new logs with the last ID migrated.
    function getOriginalID() {
       $id = intval(readConfig("id"));
@@ -47,7 +47,8 @@
    function setOriginalID($n) {
       writeConfig("id", $n);
    }
-
+   */
+  
    //Auxiliar Function to Handle DB connections
    function connectToDB($server, $user, $pass, $db) {
       $conn = new mysqli($server, $user, $pass, $db);
@@ -66,7 +67,20 @@
    */
    function migrateLogs($conn1, $conn2) {
       echo "<b>Starting Log Migration...</b> </br></br>";
-      $new_logs = $conn1->query("SELECT * FROM log WHERE log.idLog > " .getOriginalID(). ";");
+      $last_id = 0;
+      $last_id_migrated = $conn2->query("SELECT * FROM g12_log ORDER BY idLog DESC LIMIT 1");
+      
+      if ($last_id_migrated->num_rows > 0) {
+         // output data of each row
+         while($row = $last_id_migrated->fetch_assoc()) {
+             echo "<i>Last Log migrated was: " . $row["idLog"] . "</i></br>";
+             $last_id = $row["idLog"];
+         }
+      } else {
+         echo "<i>This is the first migration </i></br>";
+      }
+
+      $new_logs = $conn1->query("SELECT * FROM log WHERE log.idLog > " ./*getOriginalID()*/ $last_id . ";");
 
       if($new_logs->num_rows > 0) {
          echo "<i>Successfully imported data from origin.</i></br></br>";
@@ -82,12 +96,14 @@
                echo "Error: INSERT INTO g12_log VALUES ('" .$row["idLog"]. "', '" .$row["tabela_chave"]. "', '" .$row["coluna_chave"]. "', '" .$row["id_chave"]. "', '" .$row["valor_antigo"]. "', '" .$row["valor_novo"]. "', '" .$row["Tempo"]. "', '" .$row["Op"]. "') <br>" . $conn2->error;
             }
 
-            setOriginalID(intval($row["idLog"]));
+            //setOriginalID(intval($row["idLog"]));
          }
+      } else {
+         echo "<i>No logs to migrate.</i>";
       }
       
       echo "</br>";
-      echo "<i>Log Migration has finished</i>";
+      echo "<i>Log Migration has finished.</i>";
    }
 
    /********************************Loop in which the DB Migration logic will occur********************************/
