37afe3b7b901a364c17ce48565fa398762af4e79
Bernardo <bcmls@iscte-iul.pt>
2020-04-01 15:45:15 +0100

    import event wip

--- implementacao_g15/auditor_server/export/import_logs.bat
+++ implementacao_g15/auditor_server/export/import_logs.bat
@@ -1,1 +1,17 @@
-cd C:\xampp\mysql\bin
+
+@Echo off
+::IF NOT EXIST *.csv (
+  ::ECHO "Não tem para importar"
+  ::EXIT
+::)
+mysql --user=root auditor -e "call soma_logs" > tempfile 
+powershell -command "& {get-content tempfile |select-object -last 1}" > beforeImport
+echo beforeImport
+del tempfile
+mysql --user=root auditor < "C:\Users\Bernardo\Desktop\Iscte\3Ano\ProjetoSIDESII\implementacao_g15\auditor_server\export\insert_table.sql"
+mysql --user=root auditor -e "call importacao" > tempfile 
+echo tempfile
+
+::cleanup
+
+del beforeImport
