a7fcc5a7dda544e8dbd3872af52097664eeda936
Nuno Rego <nuno.rego@outlook.pt>
2020-04-28 16:03:32 +0100

    FireAnaliser

--- java/java/src/main/java/conn/CloudToMongo.java
+++ java/java/src/main/java/conn/CloudToMongo.java
@@ -71,6 +71,7 @@ public class CloudToMongo implements MqttCallback {
                 DBObject document_json;
 
                 document_json = (DBObject) JSON.parse(clean(c.toString()));
+                System.out.println(c.toString());
                 mongocol.insert(document_json);           
         } catch (Exception e) {
             System.out.println(e);
