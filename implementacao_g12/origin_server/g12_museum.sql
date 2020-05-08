fd33019627ecf6d897ddd5a81386856b139206eb
João Pinto <joaofranpinto@hotmail.com>
2020-05-08 22:43:23 +0100

    updated the museum mysql database

--- implementacao_g12/origin_server/g12_museum.sql
+++ implementacao_g12/origin_server/g12_museum.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 27-Abr-2020 às 20:54
+-- Tempo de geração: 08-Maio-2020 às 23:41
 -- Versão do servidor: 10.4.10-MariaDB
 -- versão do PHP: 7.1.33
 
@@ -22,6 +22,68 @@ SET time_zone = "+00:00";
 --
 -- Banco de dados: `g12_museum`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `alerta`
+--
+
+CREATE TABLE `alerta` (
+  `ID` int(11) NOT NULL,
+  `DataHoraMedicao` timestamp NULL DEFAULT NULL,
+  `TipoSensor` varchar(3) NOT NULL,
+  `ValorMedicao` decimal(6,2) NOT NULL,
+  `Limite` decimal(6,2) NOT NULL,
+  `Descricao` varchar(1000) NOT NULL,
+  `Controlo` tinyint(1) NOT NULL,
+  `Extra` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `diasemana`
+--
+
+CREATE TABLE `diasemana` (
+  `DiaSemana` varchar(20) NOT NULL,
+  `HoraRonda` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Acionadores `diasemana`
+--
+DELIMITER $$
+CREATE TRIGGER `Atualizar_DiaSemana` AFTER UPDATE ON `diasemana` FOR EACH ROW BEGIN
+	-- Find username of person performing the INSERT into table
+	SELECT user INTO @UserMail FROM (
+        SELECT user, CONCAT(user, '@', host) as userhost FROM mysql.user) base
+    WHERE userhost = USER();
+	INSERT INTO g12_logdiasemana VALUES ( DEFAULT, @UserMail, 'UPDATE', now(), new.DiaSemana, new.HoraRonda);
+END
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `Eliminar_DiaSemana` AFTER DELETE ON `diasemana` FOR EACH ROW BEGIN
+	-- Find username of person performing the INSERT into table
+	SELECT user INTO @UserMail FROM (
+        SELECT user, CONCAT(user, '@', host) as userhost FROM mysql.user) base
+    WHERE userhost = USER();
+	INSERT INTO g12_logdiasemana VALUES ( DEFAULT, @UserMail, 'DELETE', now(), old.DiaSemana, old.HoraRonda);
+END
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `Inserir_DiaSemana` AFTER INSERT ON `diasemana` FOR EACH ROW BEGIN
+	-- Find username of person performing the INSERT into table
+	SELECT user INTO @UserMail FROM (
+        SELECT user, CONCAT(user, '@', host) as userhost FROM mysql.user) base
+    WHERE userhost = USER();
+	INSERT INTO g12_logdiasemana VALUES ( DEFAULT, @UserMail, 'INSERT', now(), new.DiaSemana, new.HoraRonda);
+END
+$$
+DELIMITER ;
 
 -- --------------------------------------------------------
 
@@ -175,9 +237,235 @@ CREATE TABLE `g12_loguserhasdiasemana` (
   `DiaSemana_HoraRondaNovo` varchar(45) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `medicaosensores`
+--
+
+CREATE TABLE `medicaosensores` (
+  `idMedicao` int(11) NOT NULL,
+  `ValorMedicao` decimal(6,2) NOT NULL,
+  `TipoSensor` varchar(3) NOT NULL,
+  `DataHoraMedicao` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `PossivelAnomalia` tinyint(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Acionadores `medicaosensores`
+--
+DELIMITER $$
+CREATE TRIGGER `Atualizar_Medicoes` AFTER UPDATE ON `medicaosensores` FOR EACH ROW BEGIN
+	-- Find username of person performing the INSERT into table
+	SELECT user INTO @UserMail FROM (
+        SELECT user, CONCAT(user, '@', host) as userhost FROM mysql.user) base
+    WHERE userhost = USER();
+	INSERT INTO g12_logmedicaosensores VALUES (DEFAULT, @UserMail ,'UPDATE', now(),new.ValorMedicao, new.TipoSensor, new.DataHoraMedicao, new.PossivelAnomalia);
+END
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `Eliminar_Medicoes` AFTER DELETE ON `medicaosensores` FOR EACH ROW BEGIN
+	-- Find username of person performing the INSERT into table
+	SELECT user INTO @UserMail FROM (
+        SELECT user, CONCAT(user, '@', host) as userhost FROM mysql.user) base
+    WHERE userhost = USER();
+	INSERT INTO g12_logmedicaosensores VALUES (DEFAULT, @UserMail ,'DELETE', now(),old.ValorMedicao, old.TipoSensor, old.DataHoraMedicao, old.PossivelAnomalia);
+END
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `Inserir_Medicoes` AFTER INSERT ON `medicaosensores` FOR EACH ROW BEGIN
+	-- Find username of person performing the INSERT into table
+	SELECT user INTO @UserMail FROM (
+        SELECT user, CONCAT(user, '@', host) as userhost FROM mysql.user) base
+    WHERE userhost = USER();
+	INSERT INTO g12_logmedicaosensores VALUES ( DEFAULT, @UserMail ,'INSERT', now(),new.ValorMedicao, new.TipoSensor, new.DataHoraMedicao, new.PossivelAnomalia);
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `rondaextra`
+--
+
+CREATE TABLE `rondaextra` (
+  `dataHoraEntrada` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `EmailUtilizador` varchar(100) NOT NULL,
+  `datahoraSaida` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Acionadores `rondaextra`
+--
+DELIMITER $$
+CREATE TRIGGER `Atualizar_RondaExtra` AFTER UPDATE ON `rondaextra` FOR EACH ROW BEGIN
+	-- Find username of person performing the INSERT into table
+	SELECT user INTO @UserMail FROM (
+        SELECT user, CONCAT(user, '@', host) as userhost FROM mysql.user) base
+    WHERE userhost = USER();
+	INSERT INTO g12_logrondaextra VALUES (DEFAULT,  @UserMail , 'UPDATE', now(),new.dataHoraEntrada,new.EmailUtilizador, new.datahoraSaida);
+END
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `Eliminar_RondaExtra` AFTER DELETE ON `rondaextra` FOR EACH ROW BEGIN
+	-- Find username of person performing the INSERT into table
+	SELECT user INTO @UserMail FROM (
+        SELECT user, CONCAT(user, '@', host) as userhost FROM mysql.user) base
+    WHERE userhost = USER();
+	INSERT INTO g12_logrondaextra VALUES ( DEFAULT,  @UserMail , 'DELETE', now(),old.dataHoraEntrada,old.EmailUtilizador, old.datahoraSaida);
+END
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `Inserir_RondaExtra` AFTER INSERT ON `rondaextra` FOR EACH ROW BEGIN
+	-- Find username of person performing the INSERT into table
+	SELECT user INTO @UserMail FROM (
+        SELECT user, CONCAT(user, '@', host) as userhost FROM mysql.user) base
+    WHERE userhost = USER();
+	INSERT INTO g12_logrondaextra VALUES ( DEFAULT,  @UserMail , 'INSERT', now(),new.dataHoraEntrada, new.EmailUtilizador, new.datahoraSaida);
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `ronda_planeada`
+--
+
+CREATE TABLE `ronda_planeada` (
+  `EmailUtilizador` varchar(100) NOT NULL,
+  `DiaSemana` varchar(20) NOT NULL,
+  `HoraRondaInicio` time NOT NULL,
+  `HoraRondaSaida` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `sistema`
+--
+
+CREATE TABLE `sistema` (
+  `IDSistema` int(11) NOT NULL,
+  `IntervaloImportacaoMongo` decimal(6,2) DEFAULT NULL,
+  `TempoLimiteMedicao` int(11) DEFAULT NULL,
+  `tamanhoDosBuffersAnomalia` int(11) DEFAULT NULL,
+  `tamanhoDosBuffersAlerta` int(11) DEFAULT NULL,
+  `variacaoAnomalaTemperatura` decimal(3,2) DEFAULT NULL,
+  `variacaoAnomalaHumidade` decimal(3,2) DEFAULT NULL,
+  `variacaoAnomalaLuminosidade` decimal(3,2) DEFAULT NULL,
+  `crescimentoInstantaneoTemperatura` decimal(3,2) DEFAULT NULL,
+  `crescimentoGradualTemperatura` decimal(3,2) DEFAULT NULL,
+  `crescimentoInstantaneoHumidade` decimal(3,2) DEFAULT NULL,
+  `crescimentoGradualHumidade` decimal(3,2) DEFAULT NULL,
+  `luminosidadeLuzesDesligadas` int(11) DEFAULT NULL,
+  `limiteTemperatura` int(11) DEFAULT NULL,
+  `limiteHumidade` int(11) DEFAULT NULL,
+  `periocidadeImportacaoExportacao` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Acionadores `sistema`
+--
+DELIMITER $$
+CREATE TRIGGER `Atualizar_Sistema` AFTER UPDATE ON `sistema` FOR EACH ROW BEGIN
+	-- Find username of person performing the INSERT into table
+	SELECT user INTO @UserMail FROM (
+        SELECT user, CONCAT(user, '@', host) as userhost FROM mysql.user) base
+    WHERE userhost = USER();
+	INSERT INTO g12_logsistema VALUES ( DEFAULT, @UserMail, 'UPDATE', now(), new.IDSistema, new.LimiteTemperatura, new.LimiteHumidade, new.LimiteLuminosidade, new.LimiarTemperatura, new.LimiarHumidade, new.LimiarLuminosidade, new.DuraçãoPadrãoRonda, new.PeriocidadeImportacaoExportacao);
+END
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `Eliminar_Sistema` AFTER DELETE ON `sistema` FOR EACH ROW BEGIN
+	-- Find username of person performing the INSERT into table
+	SELECT user INTO @UserMail FROM (
+        SELECT user, CONCAT(user, '@', host) as userhost FROM mysql.user) base
+    WHERE userhost = USER();
+	INSERT INTO g12_logsistema VALUES ( DEFAULT, @UserMail, 'DELETE', now(), old.IDSistema, old.LimiteTemperatura, old.LimiteHumidade, old.LimiteLuminosidade, old.LimiarTemperatura, old.LimiarHumidade, old.LimiarLuminosidade, old.DuraçãoPadrãoRonda, old.PeriocidadeImportacaoExportacao);
+END
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `Inserir_Sistema` AFTER INSERT ON `sistema` FOR EACH ROW BEGIN
+	-- Find username of person performing the INSERT into table
+	SELECT user INTO @UserMail FROM (
+        SELECT user, CONCAT(user, '@', host) as userhost FROM mysql.user) base
+    WHERE userhost = USER();
+	INSERT INTO g12_logsistema VALUES ( DEFAULT, @UserMail, 'INSERT', now(), new.IDSistema, new.LimiteTemperatura, new.LimiteHumidade, new.LimiteLuminosidade, new.LimiarTemperatura, new.LimiarHumidade, new.LimiarLuminosidade, new.DuraçãoPadrãoRonda, new.PeriocidadeImportacaoExportacao);
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `utilizador`
+--
+
+CREATE TABLE `utilizador` (
+  `EmailUtilizador` varchar(100) NOT NULL,
+  `NomeUtilizador` varchar(200) NOT NULL,
+  `TipoUtilizador` varchar(3) NOT NULL,
+  `Morada` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Acionadores `utilizador`
+--
+DELIMITER $$
+CREATE TRIGGER `Atualizar_Utilizador` AFTER UPDATE ON `utilizador` FOR EACH ROW BEGIN
+	-- Find username of person performing the INSERT into table
+	SELECT user INTO @UserMail FROM (
+        SELECT user, CONCAT(user, '@', host) as userhost FROM mysql.user) base
+    WHERE userhost = USER();
+	INSERT INTO g12_loguser VALUES ( DEFAULT, @UserMail , 'UPDATE',now(), new.EmailUtilizador, new.NomeUtilizador, new.TipoUtilizador, new.Morada);
+END
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `Eliminar_Utilizador` AFTER DELETE ON `utilizador` FOR EACH ROW BEGIN
+	-- Find username of person performing the INSERT into table
+	SELECT user INTO @UserMail FROM (
+        SELECT user, CONCAT(user, '@', host) as userhost FROM mysql.user) base
+    WHERE userhost = USER();
+	INSERT INTO g12_loguser VALUES (DEFAULT, @UserMail , 'DELETE',now(), old.EmailUtilizador, old.NomeUtilizador, old.TipoUtilizador, old.Morada);
+END
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `Inserir_Utilizador` AFTER INSERT ON `utilizador` FOR EACH ROW BEGIN
+	-- Find username of person performing the INSERT into table
+	SELECT user INTO @UserMail FROM (
+        SELECT user, CONCAT(user, '@', host) as userhost FROM mysql.user) base
+    WHERE userhost = USER();
+	INSERT INTO g12_loguser VALUES ( DEFAULT, @UserMail , 'INSERT',now(), new.EmailUtilizador, new.NomeUtilizador, new.TipoUtilizador, new.Morada);
+END
+$$
+DELIMITER ;
+
 --
 -- Índices para tabelas despejadas
 --
+
+--
+-- Índices para tabela `alerta`
+--
+ALTER TABLE `alerta`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Índices para tabela `diasemana`
+--
+ALTER TABLE `diasemana`
+  ADD PRIMARY KEY (`DiaSemana`,`HoraRonda`),
+  ADD KEY `HoraRonda` (`HoraRonda`);
 
 --
 -- Índices para tabela `g12_logalerta`
@@ -222,6 +510,40 @@ ALTER TABLE `g12_loguserhasdiasemana`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Índices para tabela `medicaosensores`
+--
+ALTER TABLE `medicaosensores`
+  ADD PRIMARY KEY (`idMedicao`);
+
+--
+-- Índices para tabela `rondaextra`
+--
+ALTER TABLE `rondaextra`
+  ADD PRIMARY KEY (`dataHoraEntrada`),
+  ADD KEY `EmailUtilizador` (`EmailUtilizador`);
+
+--
+-- Índices para tabela `ronda_planeada`
+--
+ALTER TABLE `ronda_planeada`
+  ADD KEY `DiaSemana` (`DiaSemana`),
+  ADD KEY `EmailUtilizador` (`EmailUtilizador`),
+  ADD KEY `HoraRondaInicio` (`HoraRondaInicio`);
+
+--
+-- Índices para tabela `sistema`
+--
+ALTER TABLE `sistema`
+  ADD PRIMARY KEY (`IDSistema`);
+
+--
+-- Índices para tabela `utilizador`
+--
+ALTER TABLE `utilizador`
+  ADD PRIMARY KEY (`EmailUtilizador`),
+  ADD KEY `EmailUtilizador` (`EmailUtilizador`);
+
+--
 -- AUTO_INCREMENT de tabelas despejadas
 --
 
@@ -260,6 +582,24 @@ ALTER TABLE `g12_loguser`
 --
 ALTER TABLE `g12_loguserhasdiasemana`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restrições para despejos de tabelas
+--
+
+--
+-- Limitadores para a tabela `rondaextra`
+--
+ALTER TABLE `rondaextra`
+  ADD CONSTRAINT `rondaextra_ibfk_1` FOREIGN KEY (`EmailUtilizador`) REFERENCES `utilizador` (`EmailUtilizador`);
+
+--
+-- Limitadores para a tabela `ronda_planeada`
+--
+ALTER TABLE `ronda_planeada`
+  ADD CONSTRAINT `ronda_planeada_ibfk_1` FOREIGN KEY (`DiaSemana`) REFERENCES `diasemana` (`DiaSemana`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `ronda_planeada_ibfk_2` FOREIGN KEY (`EmailUtilizador`) REFERENCES `utilizador` (`EmailUtilizador`) ON DELETE CASCADE,
+  ADD CONSTRAINT `ronda_planeada_ibfk_3` FOREIGN KEY (`HoraRondaInicio`) REFERENCES `diasemana` (`HoraRonda`) ON DELETE CASCADE ON UPDATE CASCADE;
 SET FOREIGN_KEY_CHECKS=1;
 COMMIT;
 
