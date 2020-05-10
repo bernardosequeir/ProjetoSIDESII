4faee622058e4907989ecc5366cf937790c5674d
João Pinto <joaofranpinto@hotmail.com>
2020-05-10 23:00:41 +0100

    SP's are done

--- implementacao_g12/origin_server/g12_museum.sql
+++ implementacao_g12/origin_server/g12_museum.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 10-Maio-2020 às 23:34
+-- Tempo de geração: 10-Maio-2020 às 23:59
 -- Versão do servidor: 10.4.10-MariaDB
 -- versão do PHP: 7.1.33
 
