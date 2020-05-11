910ae51c79926abede5b6cf76206a34dfafddb61
Bernardo Sequeira <bernardosequeir@gmail.com>
2020-05-11 15:37:31 +0100

    Medicao Verifica Anomalias
    Falta implementar verificacao de datas ainda

--- java/java/src/main/java/Anomalias/Medicao.java
+++ java/java/src/main/java/Anomalias/Medicao.java
@@ -5,14 +5,45 @@ public class Medicao {
 	private double valorMedicao;
 	private String tipoSensor;
 	private String dataHoraMedicao;
-	private int possivelAnomalia;
+	private int possivelAnomalia = 0;
 	
 
-	public Medicao(String valorMedicao, String tipoSensor, String dataHoraMedicao, String possivelAnomalia) {
-		this.valorMedicao = Double.parseDouble(valorMedicao);
+	public Medicao(String valorMedicao, String tipoSensor, String dataHoraMedicao) {
+		checkTipo();
+		checkData();
+		if((tipoSensor.equals("hum")||tipoSensor.equals("lum")) && possivelAnomalia==0){
+			checkPositivo();
+		}
+		if(tipoSensor.equals("mov") && possivelAnomalia ==0){
+			checkMovimento();
+		}
 		this.tipoSensor = tipoSensor;
 		this.dataHoraMedicao = dataHoraMedicao;
-		this.possivelAnomalia = Integer.parseInt(possivelAnomalia);
+	}
+
+	private void checkPositivo() {
+		if(valorMedicao<0){
+			possivelAnomalia = 1;
+		}
+	}
+
+	private void checkMovimento(){
+		if(valorMedicao != 0.0 || valorMedicao != 1.0){
+			possivelAnomalia = 1;
+		}
+	}
+
+
+	private void checkData() {
+		//TODO Implementar um método no mongo to mysql que devolve a data do último doc inserido e compara com a deste
+	}
+
+	private void checkTipo() {
+			try {
+				this.valorMedicao = Double.parseDouble(tipoSensor);
+			} catch (Exception e) {
+				this.possivelAnomalia = 1;
+			}
 	}
 
 
