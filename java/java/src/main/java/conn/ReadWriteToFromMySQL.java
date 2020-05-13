e6b8e7cb96ea54c813bc3c628f1f4adbce44e3b9
João Pinto <joaofranpinto@hotmail.com>
2020-05-13 18:21:51 +0100

    Added auxiliary class

---
+++ java/java/src/main/java/conn/ReadWriteToFromMySQL.java
@@ -0,0 +1,12 @@
+package conn;
+
+/**
+ * 
+ * @author grupo12
+ * Auxiliary class responsible for all operations(select,insert,delete) from the mysql database
+ *
+ */
+public class ReadWriteToFromMySQL {
+	
+
+}
