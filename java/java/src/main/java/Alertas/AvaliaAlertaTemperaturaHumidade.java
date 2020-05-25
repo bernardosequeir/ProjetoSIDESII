357a486e0718e8a34ab92c845d9c1d1db92a9f3d
João Pinto <joaofranpinto@hotmail.com>
2020-05-25 18:52:45 +0100

    test push

--- java/java/src/main/java/Alertas/AvaliaAlertaTemperaturaHumidade.java
+++ java/java/src/main/java/Alertas/AvaliaAlertaTemperaturaHumidade.java
@@ -22,14 +22,14 @@ public class AvaliaAlertaTemperaturaHumidade {
 	
 	private void avaliaAlerta() {
 		if(d >= Alerta.getLimite(tipoMedicao)) {
-			Alerta.enviaAlerta();
+			//Alerta.enviaAlerta();
 		} else {
 			boolean crescimentoInstantaneo = (d / Alerta.getUltimoValor("tmp") - 1 > Alerta.getCrescimentoInstantaneo("tmp"));
 			boolean crescimentoGradual = (d / Alerta.getPrimeiroValor("tmp") - 1 > Alerta.getCrescimentoGradual("tmp"));
 			
 			if(crescimentoInstantaneo || crescimentoGradual) {
 				System.out.println(crescimentoInstantaneo+" "+crescimentoGradual);
-				Alerta.enviaAlerta();
+				//Alerta.enviaAlerta();
 			}
 		}
 	}
