6081fb70d97967ebf47be372d784c71a90a858b8
Nuno Rego <nuno.rego@outlook.pt>
2020-05-31 21:08:07 +0100

    PHP

--- implementacao_g12/origin_server/log_export/schedule_migration.bat
+++ implementacao_g12/origin_server/log_export/schedule_migration.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 
-mysql -u root -p -e "SELECT sistema.PeriocidadeImportacaoExportacao FROM sistema WHERE IDSistema = 1;" g12_museum > temp.txt
+mysql -u auditor@mail --password="" -e "SELECT sistema.PeriocidadeImportacaoExportacaoAuditor FROM sistema WHERE IDSistema = 1;" g12_museum > temp.txt
 
 more +1 <temp.txt >val_temp.txt
 
@@ -9,7 +9,7 @@ set /P p= < val_temp.txt
 del "temp.txt"
 del "val_temp.txt"
 
-schtasks /create /sc minute /mo %p% /tn "Log Migration" /tr .migrate.bat
+schtasks /create /sc minute /mo %p% /tn "Log Migration" /tr C:\xampp\htdocs\ProjetoSIDESII\implementacao_g12\origin_server\log_export\.migrate.bat
 
 ECHO Log migration scheduled to happen every %p% minutes.
 
